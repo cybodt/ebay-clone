cbd3695c67958e98bb4416fe80b929152842cf2d
Cybodt <cybodt@live.com>
2022-08-13 19:20:25 +0300

    share code on github

--- src/GlobalFooterContainer.js
+++ src/GlobalFooterContainer.js
@@ -4,10 +4,21 @@ import './GlobalFooterContainer.css';
 function GlobalFooterContainer() {
   return (
     <div className='globalFooterContainer'>
-      <div>test div</div>
       <div className='globalFooterContainer__footer'>
         <div className='globalFooterContainer__footer-menus'>
-
+          <div className='column01'>
+            <div className='buy'>
+              <h3>Buy</h3>
+              <div>Registration</div>
+              <div>eBay Money Back Guarantee</div>
+              <div>Bidding & buying help</div>
+              <div>Stores</div>
+            </div>
+          </div>
+          <div className='column02'></div>
+          <div className='column03'></div>
+          <div className='column04'></div>
+          <div className='column05'></div>
         </div>
         <div className='globalFooterContainer__footer-copyright'>
 
