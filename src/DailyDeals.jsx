1ed3a293ee99ad3bc04b0475b2c36373816203fc
Cybodt <cybodt@live.com>
2022-10-26 19:41:15 +0300

    share code on github

--- src/DailyDeals.jsx
+++ src/DailyDeals.jsx
@@ -5,14 +5,14 @@ function DailyDeals(props) {
   const { productName, srcImg, altImg, price } = props;
   return (
     <div className={productName}>
-      <div className='containerDailyDeals'>
+      <div className='daily-deals-image-container'>
         <img
-          className='imageDailyDeals'
+          className='daily-deals-image-container__image'
           src={srcImg}
           alt={altImg}
         />
       </div>
-      <div className='containerDailyDeals-price'>
+      <div className='daily-deals-price'>
         <span>{price}</span>
       </div>
     </div>
