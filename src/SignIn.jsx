1edcafc3c6ec4964823b733aa871e4f2187e28c8
Cybodt <cybodt@live.com>
2022-11-01 19:59:39 +0200

    share code on github

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -4,39 +4,39 @@ import './SignIn.css';
 
 function SignIn() {
   return (
-    <div className='signIn'>
-      <div className='signIn__survey'>
+    <div className='sign-in'>
+      <div className='sign-in__survey'>
         Tell us what you think
       </div>
-      <div className='signIn__eBayLogo'>
+      <div className='sign-in__ebay-logo'>
         <img
           src='/images/eBay_logo.jpg'
           alt='eBay logo'
         />
       </div>
-      <div className='signIn__container'>
-        <div className='signIn__container-intro'>
-          <h1 className='heading'>Hello</h1>
-          <div className='sub-heading'>
+      <div className='sign-in__container'>
+        <div className='sign-in__intro'>
+          <h1 className='sign-in__intro-heading'>Hello</h1>
+          <div className='sign-in__intro-sub-heading'>
             Sign in to eBay or&nbsp;
-            <div id='create-account-link'>
+            <div id='sign-in__create-account-link'>
               create an account
             </div>
           </div>
         </div>
-        <form className='signIn__container-form'>
-          <div className='signIn__container-form__noticeAttention'>
-            <div className='signIn__container-form__noticeAttention-icon'></div>
-            <div className='signIn__container-form__noticeAttention-text'>
+        <form className='sign-in__form'>
+          <div className='sign-in__notice-attention'>
+            <div className='sign-in__notice-attention-icon'></div>
+            <div className='sign-in__notice-attention-text'>
               Oops, that's not a match.
             </div>
           </div>
-          <div className='signIn__container-form__emailOrUsernameContainer'>
+          <div className='sign-in__email-or-username-container'>
             
           </div>
         </form>
       </div>
-      <div className='signIn__footer'>
+      <div className='sign-in__footer'>
         <FooterCopyright />
       </div>
     </div>
