cbea53c4b8c358bc991798fc8df38775ca1df357
Cybodt <cybodt@live.com>
2022-11-03 15:33:26 +0200

    share code on github

--- src/array-computer-webcams.js
+++ src/array-computer-webcams.js
@@ -1,70 +1,70 @@
 const productsComputerWebcams = [
   {
-    productName: 'logitech-C920-HD-Pro',
-    srcImg: '/images/Computer_Webcams/small_icons/Logitech_C920_HD_Pro_small.png',
+    productName: 'logitech-c920-hd-pro',
+    srcImg: '/images/Computer_Webcams/small_icons/logitech-c920-hd-pro-small.png',
     altImg: 'Logitech C920 HD Pro',
     price: '34.99',
     discount: ''
   },
   {
-    productName: '1080P-HD-Webcam-with-Microphone',
-    srcImg: '/images/Computer_Webcams/small_icons/1080P_HD_Webcam_with_Microphone_small.png',
+    productName: '1080p-hd-webcam-with-microphone',
+    srcImg: '/images/Computer_Webcams/small_icons/1080p-hd-webcam-with-microphone-small.png',
     altImg: '1080P HD Webcam with Microphone',
     price: '32.99',
     discount: ''
   },
   {
-    productName: 'lenovo-500-FHD-Webcam',
-    srcImg: '/images/Computer_Webcams/small_icons/Lenovo_500_FHD_Webcam_small.png',
+    productName: 'lenovo-500-fhd-webcam',
+    srcImg: '/images/Computer_Webcams/small_icons/lenovo-500-fhd-webcam-small.png',
     altImg: 'Lenovo 500 FHD Webcam',
     price: '25.00',
     discount: 20
   },
   {
-    productName: 'logitech-C920-HD-Pro-Webcam',
-    srcImg: '/images/Computer_Webcams/small_icons/Logitech_C920_HD_Pro_Webcam_small.png',
+    productName: 'logitech-c920-hd-pro-webcam',
+    srcImg: '/images/Computer_Webcams/small_icons/logitech-c920-hd-pro-webcam-small.png',
     altImg: 'Logitech C920 HD Pro Webcam',
     price: '55.99',
     discount: ''
   },
   {
-    productName: '1080P-Full-HD-USB-Webcam-for-PC-Desktop',
-    srcImg: '/images/Computer_Webcams/small_icons/1080P_Full_HD_USB_Webcam_for_PC_Desktop_small.png',
+    productName: '1080p-full-hd-usb-webcam-for-pc-desktop',
+    srcImg: '/images/Computer_Webcams/small_icons/1080p-full-hd-usb-webcam-for-pc-desktop-small.png',
     altImg: '1080P Full HD USB Webcam for PC Desktop',
     price: '15.69',
     discount: ''
   },
   {
-    productName: 'blue-Microphones-Snowball-Ice-with-Logitech-Pro-HD-Webcam',
-    srcImg: '/images/Computer_Webcams/small_icons/Blue_Microphones_Snowball_Ice_with_Logitech_Pro_HD_Webcam_small.png',
+    productName: 'blue-microphones-snowball-ice-with-logitech-pro-hd-webcam',
+    srcImg: '/images/Computer_Webcams/small_icons/blue-microphones-snowball-ice-with-logitech-pro-hd-webcam-small.png',
     altImg: 'Blue Microphones Snowball Ice with Logitech Pro HD Webcam',
     price: '129.99',
     discount: 15
   },
   {
-    productName: 'aluratek-4K-Ultra-HD_Live-Webcam',
-    srcImg: '/images/Computer_Webcams/small_icons/Aluratek_4K_Ultra_HD_Live_Webcam_small.png',
+    productName: 'aluratek-4k-ultra-hd-live-webcam',
+    srcImg: '/images/Computer_Webcams/small_icons/aluratek-4k-ultra-hd-live-webcam-small.png',
     altImg: 'Aluratek 4K Ultra HD Live Webcam',
     price: '68.00',
     discount: ''
   },
   {
-    productName: 'logitech-4K-Pro-Brio-Webcam',
-    srcImg: '/images/Computer_Webcams/small_icons/Logitech_4K_Pro_Brio_Webcam_small.png',
+    productName: 'logitech-4k-pro-brio-webcam',
+    srcImg: '/images/Computer_Webcams/small_icons/logitech-4k-pro-brio-webcam-small.png',
     altImg: 'Logitech 4K Pro Brio Webcam',
     price: '136.99',
     discount: ''
   },
   {
-    productName: 'logitech-Pro-4K-Webcam-Black',
-    srcImg: '/images/Computer_Webcams/small_icons/Logitech_Pro_4K_Webcam_Black_small.png',
+    productName: 'logitech-pro-4k-webcam-black',
+    srcImg: '/images/Computer_Webcams/small_icons/logitech-pro-4k-webcam-black-small.png',
     altImg: 'Logitech Pro 4K Webcam Black',
     price: '139.99',
     discount: ''
   },
   {
-    productName: 'OBSBOT-Tiny-AI-Powered-PTZ-Webcam',
-    srcImg: '/images/Computer_Webcams/small_icons/OBSBOT_Tiny_AI_Powered_PTZ_Webcam_small.png',
+    productName: 'obsbot-tiny-ai-powered-ptz-webcam',
+    srcImg: '/images/Computer_Webcams/small_icons/obsbot-tiny-ai-powered-ptz-webcam-small.png',
     altImg: 'OBSBOT Tiny AI Powered PTZ Webcam',
     price: '199.00',
     discount: 10
